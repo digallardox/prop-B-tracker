1882da8dbe256667843a7a9431671e8e9f32684f
digallardox <digallardox@gmail.com>
2021-09-01 16:08:41 -0500

    trying flexbox

--- script.js
+++ script.js
@@ -41,18 +41,18 @@ function renderArrays(x) {
 	let component = `
 	<div class="card">
             <div class="card-body">
-                <p class="headline"><b>Agency:</b> ${x.agency}</p>
-				<p class="headline"><b>Case Closed:</b> ${x.case_closed}</p>
-				<p class="headline"><b>Case Filed:</b> ${x.case_filed_date}</p>
-				<p class="headline"><b>Charge Code:</b> ${x.charge_code}</p>
-				<p class="headline"><b>Charge Description:</b> ${x.charge_description}</p>
-				<p class="headline"><b>Court Agency:</b> ${x.court_agency}</p>
-				<p class="headline"><b>Gender:</b> ${x.defendant_gender}</p>
-				<p class="headline"><b>Case Type:</b> ${x.offense_case_type}</p>
-				<p class="headline"><b>Offense Date:</b> ${x.offense_date}</p>
-				<p class="headline"><b>Street Name:</b> ${x.offense_street_name}</p>
-				<p class="headline"><b>Officer Code:</b> ${x.officer_code}</p>
-				<p class="headline"><b>Race:</b> ${x.race}</p>
+                <p><b>Agency:</b> ${x.agency}</p>
+				<p><b>Case Closed:</b> ${x.case_closed}</p>
+				<p><b>Case Filed:</b> ${x.case_filed_date}</p>
+				<p><b>Charge Code:</b> ${x.charge_code}</p>
+				<p><b>Charge Description:</b> ${x.charge_description}</p>
+				<p><b>Court Agency:</b> ${x.court_agency}</p>
+				<p><b>Gender:</b> ${x.defendant_gender}</p>
+				<p><b>Case Type:</b> ${x.offense_case_type}</p>
+				<p><b>Offense Date:</b> ${x.offense_date}</p>
+				<p><b>Street Name:</b> ${x.offense_street_name}</p>
+				<p><b>Officer Code:</b> ${x.officer_code}</p>
+				<p><b>Race:</b> ${x.race}</p>
 			
             </div>
 		</div>
